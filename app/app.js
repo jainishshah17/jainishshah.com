24d2693a1a2acb28b88c3451fdea509861afc4cf
jainish shah <jainish.shah@getzephyr.com>
2015-07-31 09:48:06 -0700

    added Contact me page

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.home',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
---
+++ app/view1/home.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'view1/home.html',
+            controller: 'homeCtrl'
+        });
+    }])
+
+    .controller('homeCtrl', [function() {
+        $scope.success = false;
+        $scope.error = false;
+        $scope.send = function () {
+
+            var htmlBody = '<div>Name: ' + $scope.user.name + '</div>' +
+                '<div>Email: ' + $scope.user.email + '</div>' +
+                '<div>Message: ' + $scope.user.body + '</div>' +
+                '<div>Date: ' + (new Date()).toString() + '</div>';
+
+            $http({
+                url: 'https://api.postmarkapp.com/email',
+                method: 'POST',
+                data: {
+                    'From': 'foo@foo.com',
+                    'To': 'bar@bar.com',
+                    'HtmlBody': htmlBody,
+                    'Subject': 'New Contact Form Submission'
+                },
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-Postmark-Server-Token': '8569dcd45-6a1a-4e7b-ae75-ea37629de4'
+                }
+            }).
+                success(function (data) {
+                    $scope.success = true;
+                    $scope.user = {};
+                }).
+                error(function (data) {
+                    $scope.error = true;
+                });
+        }
+    }]);
+
