0082e425632662419fd86263ed65723a2d98ea7d
jainish shah <jainish.shah@getzephyr.com>
2015-08-10 15:19:50 -0700

    Added About page

--- app/view1/contact.js
+++ app/view1/contact.js
@@ -12,9 +12,10 @@ angular.module('myApp.contact', ['ngRoute'])
         });
     }])
 
-    .controller('contactCtrl', [function($scope) {
+    .controller('contactCtrl', [function($scope, $http) {
+
+
         var myCenter=new google.maps.LatLng(37.543196, -121.976410);
-        //var myCenter=new google.maps.LatLng(51.508742,-0.120850);
 
             var mapProp = {
                 center:myCenter,
@@ -39,6 +40,37 @@ angular.module('myApp.contact', ['ngRoute'])
             });
 
         google.maps.event.addDomListener(window, 'load');
+
+        //$scope.success = false;
+        //$scope.error = false;
+        //$scope.send = function () {
+        //
+        //    var htmlBody = '<div>Name: ' + $scope.user.name + '</div>' +
+        //        '<div>Email: ' + $scope.user.email + '</div>' +
+        //        '<div>Message: ' + $scope.user.body + '</div>' +
+        //        '<div>Date: ' + (new Date()).toString() + '</div>';
+        //
+        //    $http({
+        //        url: 'https://api.postmarkapp.com/email',
+        //        method: 'POST',
+        //        data: {
+        //            'From': 'foo@foo.com',
+        //            'To': 'bar@bar.com',
+        //            'HtmlBody': htmlBody,
+        //            'Subject': 'New Contact Form Submission'
+        //        },
+        //        headers: {
+        //            'Accept': 'application/json',
+        //            'Content-Type': 'application/json',
+        //            'X-Postmark-Server-Token': '8569dcd45-6a1a-4e7b-ae75-ea37629de4'
+        //        }
+        //    }).success(function (data) {
+        //            $scope.success = true;
+        //            $scope.user = {};
+        //        }).error(function (data) {
+        //            $scope.error = true;
+        //        });
+        //}
 
     }]);
 
