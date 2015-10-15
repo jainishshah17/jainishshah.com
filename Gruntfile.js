30db059c282f58c815a5a3a923a06bb7bd73f3ef
jainish shah <jainish.shah@getzephyr.com>
2015-10-15 15:10:18 -0700

    Change in Build Plan

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,48 +3,31 @@
  */
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-ssh');
     grunt.initConfig({
         shell: {
             build_war: {
                 command: [
-                    'jar -cvf jainishshah.war *',
+                    'jar -cvf ROOT.war *',
                 ].join('&&')
             },
             build_docker: {
                 command: [
-                    'docker build --tag="jainishshah17/website" .',
-                    'docker push jainishshah17/website'
-                ].join('&&'),
+                    'docker build --tag="jainishshah17/website" .'
+                ],
                 options: {
                     execOptions: {
                         maxBuffer: Infinity
                     }
                 }
-            }
-        },
-        secret: grunt.file.readJSON(grunt.option("config")+'.json'),
-        sshconfig: {
-            dev: {
-                host: '<%= secret.host %>',
-                username: '<%= secret.username %>',
-                password: '<%= secret.password %>',
-                port: '<%= secret.port %>',
-                privateKey: '<%= grunt.file.read(secret.privateKey) %>',
-                passphrase: '<%= secret.passphrase %>'
-            }
-        },
-        sshexec: {
+            },
             run_xray: {
                 command: [
-                    'echo <%= secret.password %> | sudo -S whoami',
-                    'sudo docker pull jainishshah17/jainishshah.com',
-                    'sudo docker run -d -p 80:8080 --name="jainish" jainishshah17/website'
+                    'docker push jainishshah17/website',
+                    'docker run -d -p 80:8080 --name="jainish" jainishshah17/website'
                 ].join('&&')
             }
         }
     });
     grunt.registerTask('build', ['shell:build_docker']);
     grunt.registerTask('default', ['shell']);
-    grunt.registerTask('deploy',['sshexec']);
 }
