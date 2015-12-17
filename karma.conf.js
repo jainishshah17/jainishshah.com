1271dca13ff87dd353b9f945cd4e30b6b9eddee0
jainish shah <jainishshah@jainishs-MacBook-Pro.local>
2015-12-16 20:09:38 -0800

    removed app from context path

--- karma.conf.js
+++ karma.conf.js
@@ -4,9 +4,9 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
     ],
